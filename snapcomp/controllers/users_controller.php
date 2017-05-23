1d4bfd1a03b64ede4469e1c1c004b2d46d734b66
TV\Tristan <tristan.visnar@student.um.si>
2017-05-23 23:52:21 +0200

    RegisterFinalCommit

--- snapcomp/controllers/users_controller.php
+++ snapcomp/controllers/users_controller.php
@@ -16,14 +16,14 @@
     	public function profileUser() {
 			//če uporabnik ne posreduje ID uporabnika, pokličemo akcijo napaka
 			if(!isset($_SESSION["ID"])){
-        return call("pages","login");
+        header("?controller=pages&action=login");
 				//require_once("views/users/login.php");
 			}
 			else{
 				$user = User::find($_SESSION["ID"]);
 				if($user == "false"){
 			//ta uporabnik ne obstaja -
-					return call('pages', 'error'); /// možen klic tudi bolj prilagojene funkcije - uporabnik ne obstaja
+					header("?controller=pages&action=error"); /// možen klic tudi bolj prilagojene funkcije - uporabnik ne obstaja
 				}
 				else
 				{
@@ -47,47 +47,39 @@
     //če bo ključ prekoračil 1000 zahtev na mesec, si na strani ustvarite svoj ključ
     //na strani je tudi opisan format odgovora, nas bo zanimala predvsem mx_found lastnost
 
-        echo "start";
+
     		$ret_email = User::check_EMAIL($email);      /// preveri EMAIL
-        echo "check email ... OK <br/>";
     		$user = $_POST["regAccountName"];
     		$ret_user = User::check_ACC_NAME($user);     /// preveri ACC_NAME
-        echo "check acc_name ... OK <br/>";
     		if($ret_email == "error" || $ret_user == "error")
-      			return call('pages', 'error');
-        echo "check errors ... OK <br/>";
+      			header("?controller=pages&action=error");
         if($ret_email != "false" && $ret_user!="false"){
             header("?controller=pages&action=register&email=$email&acc=$user");
         }
-        echo "check email_acc_set ... OK <br/>";
     		if($ret_email != "false"){
             header("?controller=pages&action=register&email=$email");
         //KAJ NAREDI V PRIMERU KO MAIL ŽE OBSTAJA
     		}
-        echo "check email_set ... OK <br/>";
     		if($ret_user != "false"){
             header("?controller=pages&action=register&acc=$user");
         //KAJ NAREDI V PRIMERU KO ACC_NAME ŽE OBSTAJA
     		}
-        echo "check acc_set ... OK <br/>";
     		if($ret_email == "false" && $ret_user == "false"){
         //SE ZGODI OB USPEŠNI PREVERITVI PODATKOV
   			    $user=User::save($_POST["regEmail"],$_POST["regAccountName"],$_POST["regUsername"],$_POST["regPassword"],$_POST["regBirthDate"],$_POST["regFirstName"],$_POST["regLastName"],$_POST["regCountry"],$_POST["optradio1"],$_POST["optradio2"]);
             if($user=="error"){
-                echo "adding user ... error <br/>";
-                return call("pages","error");
+                header("?controller=pages&action=error");
             }else{
-                echo "adding user ... OK <br/>";
                 $_SESSION["ID"] = $user["ID"];
         				$_SESSION["USERNAME"] = $user["USERNAME"];
         				$_SESSION["ACCNAME"] = $user["ACCNAME"];
         				$_SESSION["USERNAME"] = $user["USERNAME"];
         				$_SESSION["TEST"] = $user["LANG"];
   			//naložimo pogled, ki potrjuje uspešnost dodajanja
-  		          return call("pages","home");
+  		          header("?controller=pages&action=home");
             }
    	    }
-        return call("pages","error");
+        header("?controller=pages&action=error");
     }
 
 
@@ -95,7 +87,7 @@
 			$id = User::login($_POST["ACCNAME"],$_POST["PASS"]);
 			var_dump($_POST);
 			if($id == "error"){
-				return call('pages','error');
+				header("?controller=pages&action=error");
 			}
 			if($id == "false"){
 				//error napačna prijava -- preusmeri nazaj na
@@ -109,7 +101,7 @@
 				$_SESSION["ACCNAME"] = $id["ACCNAME"];
 				$_SESSION["USERNAME"] = $id["USERNAME"];
 				$_SESSION["TEST"] = $id["LANG"];
-				return call('pages','home'); // vrne nas na home page;
+				header("?controller=pages&action=home"); // vrne nas na home page;
 			}
 		}
 
