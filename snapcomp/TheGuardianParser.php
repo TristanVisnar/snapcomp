b35ad51547f07db218d0bf4ee7db1989148495fb
Catacomba <kevinsuc16@gmail.com>
2017-05-24 16:34:51 +0200

    asda

--- snapcomp/TheGuardianParser.php
+++ snapcomp/TheGuardianParser.php
@@ -1,9 +1,9 @@
 <?php 
 	echo "PraviPHp";
-	if(function_exists("file_get_html")){
-		echo "true";
-	}
-	else echo "Ne obstaja";
+	//if(function_exists("file_get_html")){
+	//	echo "true";
+	//}
+	//else echo "Ne obstaja";
 
 	/*$html = file_get_html("https://www.theguardian.com/international");
 	$ret = $html->find('span[class=fc-item__kicker]'); 
