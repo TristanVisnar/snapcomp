b6d63a38c0347e0cd37684039e671bd690b5dbca
Catacomba <kevinsuc16@gmail.com>
2017-05-24 17:17:34 +0200

    Kommitor

--- snapcomp/TheGuardianParser.php
+++ snapcomp/TheGuardianParser.php
@@ -1,5 +1,5 @@
 <?php 
-	include('/var/www/html/parser/pharse/pharse.php');
+	include('../parser/pharse/pharse.php');
 	echo "Pharser zagon <br>";
 	$html = Pharse::file_get_dom('https://www.theguardian.com/international');
 	// Find all the paragraph tags with a class attribute and print the
