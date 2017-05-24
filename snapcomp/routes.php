def39f622331c7eeaa186822d1834208f2f6cbc4
Catacomba <kevinsuc16@gmail.com>
2017-05-24 09:17:08 +0200

    logoutfix1

--- snapcomp/routes.php
+++ snapcomp/routes.php
@@ -21,8 +21,8 @@
 
   //vsi dovoljeni kontrolerji, v našem primeru 2
   //tukaj lahko dodamo tudi avtentikacijo (preverjamo, če je uporabnik v seji in ali ima pravice izvesti določeno akcijo)
-   $controllers = array('pages' => ['home', 'error', 'browse', 'login', 'register', 'NSFW', 'profile', 'logout'],
-                        'users' => ['profileUser', 'add','register','login', 'redirect'],
+   $controllers = array('pages' => ['home', 'error', 'browse', 'login', 'register', 'NSFW', 'profile'],
+                        'users' => ['profileUser', 'add','register','login', 'redirect','logout'],
 			'images' => ['browse', 'user','like','dislike']
 	       	       );
   if (array_key_exists($controller, $controllers)) {
