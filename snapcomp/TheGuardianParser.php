95025e8446b1601a0b113f24dc2e2fd0909fcb7c
Catacomba <kevinsuc16@gmail.com>
2017-05-24 17:09:55 +0200

    Tojeto

--- snapcomp/TheGuardianParser.php
+++ snapcomp/TheGuardianParser.php
@@ -1,13 +1,10 @@
 <?php 
-	echo "PraviPHp";
-	if(function_exists("file_get_html")){
-		echo "true";
-	}
-	else echo "Ne obstaja";
+	include('/var/www/html/parser/pharse/pharse.php');
 
-	/*$html = file_get_html("https://www.theguardian.com/international");
-	$ret = $html->find('span[class=fc-item__kicker]'); 
-	$text = $ret->plaintext; 
-	var_dump($ret);
-	var_dump($text);*/
+	$html = Pharse::file_get_dom('https://www.theguardian.com/international');
+	// Find all the paragraph tags with a class attribute and print the
+// value of the class attribute
+	foreach($html('span[class="fc-item__kicker"]') as $element) {
+		echo $element->getPlainText(), "<br>\n"; 
+	}
 ?>
