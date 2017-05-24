701921ab37916ad83eb058d13bd8a536e0a7cd95
Catacomba <kevinsuc16@gmail.com>
2017-05-24 16:37:35 +0200

    tapravikomit

--- TheGuardianParser.php
+++ TheGuardianParser.php
@@ -1,11 +1,13 @@
 <?php 
+	echo "PraviPHp";
+	//if(function_exists("file_get_html")){
+	//	echo "true";
+	//}
+	//else echo "Ne obstaja";
 
-	$html = file_get_html("https://www.theguardian.com/international");
-	if(function_exists(file_get_html)){
-		echo "true";
-	}
+	/*$html = file_get_html("https://www.theguardian.com/international");
 	$ret = $html->find('span[class=fc-item__kicker]'); 
 	$text = $ret->plaintext; 
 	var_dump($ret);
-	var_dump($text);
+	var_dump($text);*/
 ?>
