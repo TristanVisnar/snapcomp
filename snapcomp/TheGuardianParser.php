621c31614d4977ddde0f4d0d1db2bc9acb31339b
Catacomba <kevinsuc16@gmail.com>
2017-05-24 16:03:36 +0200

    ParserGuardianKomit1

---
+++ snapcomp/TheGuardianParser.php
@@ -0,0 +1,9 @@
+<?php 
+
+$html = file_get_html("https://www.theguardian.com/international");
+
+$ret = $html->find('span[class=fc-item__kicker]'); 
+$text = $ret->plaintext; 
+var_dump($ret);
+var_dump($text);
+?>
