b400219d378df22c3b1eec82454ef0642c0c5b35
Catacomba <kevinsuc16@gmail.com>
2017-05-26 14:45:17 +0200

    sdfasd

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -4,22 +4,19 @@
 	
 	//https://www.reddit.com/r/random
 //https://www.reddit.com/r/wholesomeoverwatch/
-	for ($x = 0; $x <= 1; $x++) {
+	for ($x = 0; $x <= 100; $x++) {
 		//$html = file_get_contents('https://www.reddit.com/r/random');
-		//$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-		//file_get_contents("http://www.something.com/somepage.html",false,$context);
-		$html = file_get_contents("https://www.reddit.com/r/random");
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
 		//echo $x;
 		//echo "<!--" .$html. "-->";
 		//echo "_________________________________________________________________________________________\n<br>";
 		foreach($html('title') as $element) {
 			echo "Title [".$x."]:  ". $element->getPlainText(), "<br>\n";
-			//file_put_contents("Teme.txt", $Title, FILE_APPEND | LOCK_EX);				
+			//file_put_contents($, $person, FILE_APPEND | LOCK_EX);			
 		}
 		if(isset($html)){
 			unset($html);
 		}
 	}
-
 	echo "Pharser konec";
 ?>
