96a95f3bff7cc55141998cd5770252f4f90f03e2
Catacomba <kevinsuc16@gmail.com>
2017-05-26 11:56:32 +0200

    Redditrandomparser

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -1,9 +1,13 @@
 <?php 
 	include('../../parser/pharse/pharse.php');
-	echo "TheGuardian Parser <br>";
-	$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
-	// Find all the paragraph tags with a class attribute and print the
-// value of the class attribute
-	echo $html;
+	echo "Reddit random parser<br>";
+	
+	
+	for ($x = 0; $x <= 100; $x++) 
+	{
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		$naslov = $html["title"];
+		echo $naslov . "<br>\n";
+	} 
 	echo "Pharser konec";
 ?>
