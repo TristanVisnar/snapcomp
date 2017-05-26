098a2fa3bff22ab9299759340c2ef27da86ce0e7
Catacomba <kevinsuc16@gmail.com>
2017-05-26 13:53:20 +0200

    testse

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -1,21 +1,22 @@
 <?php 
 	include('../../parser/pharse/pharse.php');
-	echo "<!--Reddit random parser<br>";
+	echo "Reddit random parser<br>";
 	
 	//https://www.reddit.com/r/random
 //https://www.reddit.com/r/wholesomeoverwatch/
-	for ($x = 0; $x <= 4; $x++) {
+	for ($x = 0; $x <= 20; $x++) {
 		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		echo $x;
 		//echo "<!--" .$html. "-->";
 		//echo "_________________________________________________________________________________________\n<br>";
-		foreach($html('title') as $element) {
-			echo "Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
-		}
-		if(isset($html)){
-			unset($html);
-		}
+		//foreach($html('title') as $element) {
+		//	echo "Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
+		//}
+		//if(isset($html)){
+		//	unset($html);
+		//}
 	}
 
 
-	echo "Pharser konec-->";
+	echo "Pharser konec";
 ?>
