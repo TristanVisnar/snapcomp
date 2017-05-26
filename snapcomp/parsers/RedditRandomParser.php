1a46902226a6ab552f9c8a63f7a9c01f8a95fdf0
Catacomba <kevinsuc16@gmail.com>
2017-05-26 12:24:56 +0200

    2342

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -2,13 +2,13 @@
 	include('../../parser/pharse/pharse.php');
 	echo "Reddit random parser<br>";
 	
-	
-
+	//https://www.reddit.com/r/random
+https://www.reddit.com/r/wholesomeoverwatch/
 		echo "Title: ";
-		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
-		echo "<!--".$html."-->";
-		//$naslov = $html["title"];
-		//echo $naslov . "<br>\n";
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/wholesomeoverwatch/');
+		//echo "<!--".$html."-->";
+		$naslov = $html("title");
+		echo $naslov . "<br>\n";
 
 	echo "Pharser konec";
 ?>
