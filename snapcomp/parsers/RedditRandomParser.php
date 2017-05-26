00c12749a0bd40ee5b8100679a0b0aee5fe270e6
Catacomba <kevinsuc16@gmail.com>
2017-05-26 16:11:28 +0200

    FinalParsersKomit

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -1,42 +1,58 @@
 <?php 
 	include('../../parser/pharse/pharse.php');
-	echo "Reddit random parser<br>";
 	
-	
+	//Konekcija
 	$servername = "localhost";
 	$username = "user";
 	$password = "joomladb";
 	$dbname = "snapcomp";
-
-	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-	//https://www.reddit.com/r/random
-	//https://www.reddit.com/r/wholesomeoverwatch/
-	for ($x = 0; $x <= 2; $x++) {
-		//$html = file_get_contents('https://www.reddit.com/r/random');
+	//Reddit random parser (počasen ko pes)
+	for ($x = 0; $x <= 50; $x++) {
 		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
-		//echo $x;
-		//echo "<!--" .$html. "-->";
-		//echo "_________________________________________________________________________________________\n<br>";
 		foreach($html('title') as $element) {
-			$Tstring = "Title [".$x."]:  ". $element->getPlainText() . "<br>\n";
-			$sql = "INSERT INTO SUGGESTION (INFO, SOURCE) VALUES ('".$element->getPlainText()."','RedditRandom')";
+			$sql = "INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES ('".$element->getPlainText()."','RedditRandom')";
 			if ($conn->query($sql) === TRUE) {
 				echo "Vnos ".$x." uspel!";
 			} else {
 				echo "Error: " . $sql . "<br>" . $conn->error;
 			}
-			echo $Tstring;
 		}
 		if(isset($html)){
 			unset($html);
 		}
 		
 	}
+	echo "Reddit//random parser finished!";
+	//The sun Parser
+	$x = 1;
+	$html = Pharse::file_get_dom('https://www.thesun.co.uk/');
+	foreach($html('h2[class="teaser__headline theme__copy-color"]') as $element) {
+		$sql = "INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES ('".$element->getPlainText()."','thesun.co.uk')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Vnos ".$x." uspel!";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		$x++;
+	}
+	echo "thesun.co.uk parser finished!"
+	//The guardian parser
+	$x = 1;
+	$html = Pharse::file_get_dom('https://www.theguardian.com/international');
+	foreach($html('span[class="fc-item__kicker"]') as $element) {
+		$sql = "INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES ('".$element->getPlainText()."','theguardian.com')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Vnos ".$x." uspel!";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}		
+		$x++;
+	}
+	echo "theguardian.com parser finished!";
 	$conn->close();
-	echo "Pharser konec";
+	echo "All parsers finished. The database should be updated.";
 ?>
