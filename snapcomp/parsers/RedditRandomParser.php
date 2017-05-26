1d85119e4e6ea37faca0be6e780f5c5f5e138bff
Catacomba <kevinsuc16@gmail.com>
2017-05-26 12:01:09 +0200

    Redditkomit1

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -5,6 +5,7 @@
 	
 	for ($x = 0; $x <= 100; $x++) 
 	{
+		echo "Title: ";
 		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
 		$naslov = $html["title"];
 		echo $naslov . "<br>\n";
