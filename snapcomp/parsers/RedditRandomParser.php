5ba76e851b311881f64bab60835594ef4be7c2f2
Catacomba <kevinsuc16@gmail.com>
2017-05-26 13:28:46 +0200

    asasd

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -4,12 +4,34 @@
 	
 	//https://www.reddit.com/r/random
 //https://www.reddit.com/r/wholesomeoverwatch/
-	for ($x = 0; $x <= 100; $x++) {
+	for ($x = 0; $x <= 2; $x++) {
 		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
 		foreach($html('title') as $element) {
 			echo "Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
 		}	
 	}
+	
+	for ($x = 0; $x <= 4; $x++) {
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		foreach($html('title') as $element) {
+			echo "1Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
+		}	
+	}
+
+	for ($x = 0; $x <= 6; $x++) {
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		foreach($html('title') as $element) {
+			echo "2Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
+		}	
+	}
+
+	for ($x = 0; $x <= 10; $x++) {
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		foreach($html('title') as $element) {
+			echo "3Title [".$x."]:  ". $element->getPlainText(), "<br>\n"; 
+		}	
+	}
+
 
 	echo "Pharser konec";
 ?>
