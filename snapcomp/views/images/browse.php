8aacda3679db6b4491e6e2269224c504c2f02de8
ApollyonDS <miha.vres69@gmail.com>
2017-05-26 15:04:56 +0200

    browse

---
+++ snapcomp/views/images/browse.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script></head>
+
+  </head>
+  <body>
+    <div class="row">
+      <div class="col" style="width:500px">
+        <ul class="list-group">
+          <li style="padding-top:0px; padding-bottom:0px" class="list-group-item">
+            <font size="5px">&nbsp;TITLE OF POST (Theme)</font>
+          </li>
+
+          <li style="padding:0px;" class="list-group-item">
+            <img src="http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg" alt="" width="100%" height="350px">
+          </li>
+
+          <li class="list-group-item">
+            <b>VOTE:&nbsp;</b>
+            <button style="margin-bottom:4px" class="btn btn-success btn-xs glyphicon glyphicon-arrow-up"></button>
+            <?php
+             echo "234";
+            ?>
+            <button style="margin-bottom:4px" class="btn btn-danger btn-xs glyphicon glyphicon-arrow-down"></button>
+            &nbsp;&nbsp;&nbsp;&nbsp;<b>WINNER: </b>Username12345
+            <div style="float:right; margin-top:1px">&nbsp;&nbsp;<b>NAME: </b>Room12345</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </body>
+</html>
