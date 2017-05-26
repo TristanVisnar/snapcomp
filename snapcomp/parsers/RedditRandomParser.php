426f6ce29cd301bf55df8ad572d884689b2ad35c
Catacomba <kevinsuc16@gmail.com>
2017-05-26 12:07:03 +0200

    regerg

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -3,12 +3,11 @@
 	echo "Reddit random parser<br>";
 	
 	
-	for ($x = 0; $x <= 100; $x++) 
-	{
+
 		echo "Title: ";
-		//$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
-		//$naslov = $html["title"];
-		//echo $naslov . "<br>\n";
-	} 
+		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
+		$naslov = $html["title"];
+		echo $naslov . "<br>\n";
+
 	echo "Pharser konec";
 ?>
