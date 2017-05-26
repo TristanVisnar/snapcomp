bed737296fa0f496d9d57266ea72bb77a7ab913b
Catacomba <kevinsuc16@gmail.com>
2017-05-26 15:45:24 +0200

    BazaupdateCommit

--- snapcomp/parsers/RedditRandomParser.php
+++ snapcomp/parsers/RedditRandomParser.php
@@ -2,9 +2,21 @@
 	include('../../parser/pharse/pharse.php');
 	echo "Reddit random parser<br>";
 	
+	
+	$servername = "localhost";
+	$username = "user";
+	$password = "joomladb";
+	$dbname = "snapcomp";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 	//https://www.reddit.com/r/random
-//https://www.reddit.com/r/wholesomeoverwatch/
-	for ($x = 0; $x <= 0; $x++) {
+	//https://www.reddit.com/r/wholesomeoverwatch/
+	for ($x = 0; $x <= 2; $x++) {
 		//$html = file_get_contents('https://www.reddit.com/r/random');
 		$html = Pharse::file_get_dom('https://www.reddit.com/r/random');
 		//echo $x;
@@ -12,11 +24,19 @@
 		//echo "_________________________________________________________________________________________\n<br>";
 		foreach($html('title') as $element) {
 			$Tstring = "Title [".$x."]:  ". $element->getPlainText() . "<br>\n";
+			$sql = "INSERT INTO SUGGESTION (INFO, SOURCE) VALUES ('".$element->getPlainText()."','RedditRandom')";
+			if ($conn->query($sql) === TRUE) {
+				echo "Vnos ".$x." uspel!";
+			} else {
+				echo "Error: " . $sql . "<br>" . $conn->error;
+			}
 			echo $Tstring;
 		}
 		if(isset($html)){
 			unset($html);
 		}
+		
 	}
+	$conn->close();
 	echo "Pharser konec";
 ?>
