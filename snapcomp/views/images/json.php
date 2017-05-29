441ac4cf0c60f9795747513259b4d68701f22c11
TV\Tristan <tristan.visnar@student.um.si>
2017-05-29 18:05:45 +0200

    API Browse v4.2

--- snapcomp/views/images/json.php
+++ snapcomp/views/images/json.php
@@ -1,7 +1,6 @@
 <?php
 //pogled za vračanje oglasa v json obliki
 //nastavimo ustrezen tip rezultata ter oglas ustrezno zakodiramo
-echo "SEM V DATOTEKI";
 header('Content-Type: application/json');
 foreach ($slike as $slika) {
   echo json_encode($slika);
