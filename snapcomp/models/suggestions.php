660af65bf5ee09bd95c0a17f52b2ad64d63f2fad
TV\Tristan <tristan.visnar@student.um.si>
2017-06-01 12:09:09 +0200

    flipin buggs

--- snapcomp/models/suggestions.php
+++ snapcomp/models/suggestions.php
@@ -7,7 +7,7 @@ class Suggestion{
 
   public function getDalySuggestions(){
     $db = Db::getInstance();
-    $result = mysqli_query($db,"SELECT * FROM DAILY_SUGGESTION;")
+    $result = mysqli_query($db,"SELECT * FROM DAILY_SUGGESTION;");
     $list = [];
     while($row = mysqli_fetch_assoc($result)){
        $list[] = array("ID" => $row['ID'], "INFO" => $row['INFO'],"SOURCE"=>$row['SOURCE']);
