433449b08f621b9d513a2fd6dbb50cce0967587f
Catacomba <kevinsuc16@gmail.com>
2017-06-01 18:01:29 +0200

    asd

--- snapcomp/models/rooms.php
+++ snapcomp/models/rooms.php
@@ -78,6 +78,7 @@ class Room{
 	
 	public function updateSessionTheme($id_session, $suggestion_info){
 		$db = Db::getInstance();
+		echo "SUGGESTION INFO: ". $suggestion_info;
 		if ($stmt = mysqli_prepare($db, "UPDATE SESSION SET ID_SUGGESTION = (SELECT ID FROM SUGGESTION WHERE INFO = ?) WHERE ID = ?")) {
 			mysqli_stmt_bind_param($stmt, "Si",$suggestion_info,intval($id_session));
 			mysqli_stmt_execute($stmt);
