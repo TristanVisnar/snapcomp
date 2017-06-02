1b609362c9a76f4021bfbcb381e4bb8eb5a5ee86
Catacomba <kevinsuc16@gmail.com>
2017-06-02 20:28:05 +0200

    dsd

--- snapcomp/controllers/rooms_controller.php
+++ snapcomp/controllers/rooms_controller.php
@@ -60,6 +60,7 @@ class rooms_controller{
 	}
 	 public function postAPI($request,$input){
         if($request[1]=="createRoom"){
+			return "Sm v apiju";
 			echo "KreiralRoom";
 			rooms_controller::createRoom($request,$input);
 			header('Content-Type: application/json');
