c72ac5414c1cad149b7531c41ce780a99d8c7c8c
Catacomba <kevinsuc16@gmail.com>
2017-06-03 17:16:29 +0200

    asda
    sad

--- snapcomp/controllers/rooms_controller.php
+++ snapcomp/controllers/rooms_controller.php
@@ -62,8 +62,8 @@ class rooms_controller{
         if($request[1]=="createRoom"){
 			$id = rooms_controller::createRoom($request,$input);
 			header('Content-Type: application/json');
-			//echo $id;
-			echo json_encode($id);
+			echo $id;
+			//echo json_encode($id);
 		}
     }
 
