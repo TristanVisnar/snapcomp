52f8877f8dc805d9c4587b9d5320ec4181195916
TV\Tristan <tristan.visnar@student.um.si>
2017-06-04 14:18:15 +0200

    adksdkčkačwkpqkdmn nlmc

--- snapcomp/models/timer.php
+++ snapcomp/models/timer.php
@@ -3,16 +3,18 @@
 class Timer{
 
   public function getDateOfStart($session_id){
-
+    echo "in date of start";
     $db = Db::getInstance();
 
     if ($stmt = mysqli_prepare($db, "SELECT ses.DATEOFSTART,r.GAME_STATE,NOW() as NOW FROM SESSION as ses and ROOM as r where ses.ID_ROOM=r.ID and ses.ID=? ;")) {
       mysqli_stmt_bind_param($stmt, "i",$session_id);
       //izvedemo poizvedbo
       mysqli_stmt_execute($stmt);
+      echo "kaoc sas dasad";
       $result = mysqli_stmt_get_result($stmt);
       mysqli_stmt_close($stmt);
       if($row = mysqli_fetch_assoc($result)){
+        echo "row get";
          return array("DATEOFSTART"=>$row["DATEOFSTART"],"GAME_STATE"=>$row["GAME_STATE"],"NOW"=>$row["NOW"]);
       }
       return "error: does not exist";
