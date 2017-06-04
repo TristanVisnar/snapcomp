f4083e0ebfa4805c02972c9866b67ad55292d3cb
TV\Tristan <tristan.visnar@student.um.si>
2017-06-04 15:43:42 +0200

    pkapdp

--- snapcomp/models/timer.php
+++ snapcomp/models/timer.php
@@ -5,14 +5,14 @@ class Timer{
   public function getDateOfStart($session_id){
     $db = Db::getInstance();
 
-    if ($stmt = mysqli_prepare($db, "SELECT ses.DATEOFSTART,r.GAMESTATE,NOW() as NV FROM SESSION as ses, ROOM as r where ses.ID_ROOM=r.ID and ses.ID=? ;")) {
+    if ($stmt = mysqli_prepare($db, "SELECT (NOW() - ses.DATEOFSTART) as TIMEGOING ,r.GAMESTATE FROM SESSION as ses, ROOM as r where ses.ID_ROOM=r.ID and ses.ID=? ;")) {
       mysqli_stmt_bind_param($stmt, "i",$session_id);
       //izvedemo poizvedbo
       mysqli_stmt_execute($stmt);
       $result = mysqli_stmt_get_result($stmt);
       mysqli_stmt_close($stmt);
       if($row = mysqli_fetch_assoc($result)){
-         return array("DATEOFSTART"=>$row["DATEOFSTART"],"GAMESTATE"=>$row["GAMESTATE"],"NOW"=>$row["NV"]);
+         return array("GAMESTATE"=>$row["GAMESTATE"], "TIMEGOING"=>$row["TIMEGOING"]);
       }
       return "error: does not exist";
     }
