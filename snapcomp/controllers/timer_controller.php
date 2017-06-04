b903bbca692d3241e0f40531d2c2a96e00e55956
TV\Tristan <tristan.visnar@student.um.si>
2017-06-04 14:48:54 +0200

    kakajpodkjkljdkljačkls

--- snapcomp/controllers/timer_controller.php
+++ snapcomp/controllers/timer_controller.php
@@ -7,14 +7,12 @@ class timer_controller{
   //output:
   // DATEOFSTART, STATE , NOW - server time;
   public function getSessionStart($request,$input){
-    echo "In session start";
     $out = Timer::getDateOfStart($request[2]);
     echo json_encode($out);
   }
 
 
   public function getAPI($request,$input){
-    echo "In appi";
     if($request[1]=="getStart"){
       timer_controller::getSessionStart($request,$input);
     }
--- snapcomp/models/timer.php
+++ snapcomp/models/timer.php
@@ -3,19 +3,16 @@
 class Timer{
 
   public function getDateOfStart($session_id){
-    echo "in date of start";
     $db = Db::getInstance();
 
-    if ($stmt = mysqli_prepare($db, "SELECT ses.DATEOFSTART,r.GAME_STATE,NOW() as NOW FROM SESSION as ses and ROOM as r where ses.ID_ROOM=r.ID and ses.ID=? ;")) {
+    if ($stmt = mysqli_prepare($db, "SELECT ses.DATEOFSTART,r.GAMESTATE,NOW() as NV FROM SESSION as ses, ROOM as r where ses.ID_ROOM=r.ID and ses.ID=? ;")) {
       mysqli_stmt_bind_param($stmt, "i",$session_id);
       //izvedemo poizvedbo
       mysqli_stmt_execute($stmt);
-      echo "kaoc sas dasad";
       $result = mysqli_stmt_get_result($stmt);
       mysqli_stmt_close($stmt);
       if($row = mysqli_fetch_assoc($result)){
-        echo "row get";
-         return array("DATEOFSTART"=>$row["DATEOFSTART"],"GAME_STATE"=>$row["GAME_STATE"],"NOW"=>$row["NOW"]);
+         return array("DATEOFSTART"=>$row["DATEOFSTART"],"GAMESTATE"=>$row["GAMESTATE"],"NOW"=>$row["NV"]);
       }
       return "error: does not exist";
     }
