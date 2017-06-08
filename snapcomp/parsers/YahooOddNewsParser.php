bd873ec97a7bcb1135af20082f1201f118dbea71
Catacomba <kevinsuc16@gmail.com>
2017-06-08 09:09:34 +0200

    sd

--- snapcomp/parsers/YahooOddNewsParser.php
+++ snapcomp/parsers/YahooOddNewsParser.php
@@ -6,7 +6,7 @@
 // value of the class attribute
 	$returnList = [];
 	foreach($html('span[class="Fw(b) Fz(20px) Lh(23px) LineClamp(2,46px) Fz(17px)--sm1024 Lh(19px)--sm1024 LineClamp(2,38px)--sm1024 Td(n) C(#0078ff):h C(#000)"]') as $element) {
-		$vnos = $element->getPlainText(), "<br>\n"; 
+		$vnos = $element->getPlainText(); 
 		str_ireplace(' news', '', $vnos);
 		str_ireplace(' news ', '', $vnos);
 		if(str_word_count($vnos)>4){
