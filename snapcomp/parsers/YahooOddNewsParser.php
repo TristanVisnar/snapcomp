b0f1805e6b0186f294de101f4edca9c728673496
Catacomba <kevinsuc16@gmail.com>
2017-06-08 14:08:36 +0200

    vv

--- snapcomp/parsers/YahooOddNewsParser.php
+++ snapcomp/parsers/YahooOddNewsParser.php
@@ -11,6 +11,7 @@
 		str_ireplace(' news', '', $vnos);
 		str_ireplace(' news ', '', $vnos);
 		str_ireplace('-- react-text --', '', $vnos);
+		str_ireplace('--/react-text --', '', $vnos);
 		if(str_word_count($vnos)>4){
 			$first4words = implode(' ', array_slice(str_word_count($vnos,1), 0, 4));
 			$vnos = $first4words;
