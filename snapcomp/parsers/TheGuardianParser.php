e6d5a89b751a8ac510f46dc7d812f04c4c9deff8
Kevin <kevinsuc16@gmail.com>
2017-06-09 11:16:08 +0200

    asd

--- snapcomp/parsers/TheGuardianParser.php
+++ snapcomp/parsers/TheGuardianParser.php
@@ -1,6 +1,7 @@
 <?php
+//include('../../parser/pharse/pharse.php');
 function TheGuardianParser(){
-	include('../../parser/pharse/pharse.php');
+	//include('../../parser/pharse/pharse.php');
 	echo "TheGuardian Parser <br>";
 	$html = Pharse::file_get_dom('https://www.theguardian.com/international');
 	// Find all the paragraph tags with a class attribute and print the
