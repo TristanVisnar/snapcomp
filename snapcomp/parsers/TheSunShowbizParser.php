647f82bfd9b91751dc0517b103039cd70ece617f
Kevin <kevinsuc16@gmail.com>
2017-06-09 15:07:24 +0200

    ¸gh

--- snapcomp/parsers/TheSunShowbizParser.php
+++ snapcomp/parsers/TheSunShowbizParser.php
@@ -1,5 +1,5 @@
 <?php
-  include('../../parser/pharse/pharse.php');
+  //include('../../parser/pharse/pharse.php');
 function TheSunShowbizzParser(){
 	//include('../../parser/pharse/pharse.php');
 	//echo "The Sun Showbizz parser <br>";
@@ -24,5 +24,5 @@ function TheSunShowbizzParser(){
 	var_dump($new_arr);
 	return $new_arr;
 }
-TheSunShowbizzParser();
+//TheSunShowbizzParser();
 ?>
