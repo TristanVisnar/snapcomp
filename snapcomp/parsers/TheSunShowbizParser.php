30913bd8c66abd8fce766fde50d33f90c758701b
Kevin <kevinsuc16@gmail.com>
2017-06-09 09:12:05 +0200

    asd

--- snapcomp/parsers/TheSunShowbizParser.php
+++ snapcomp/parsers/TheSunShowbizParser.php
@@ -1,35 +1,35 @@
 <?php
 	include('../../parser/pharse/pharse.php');
-	echo "Yahoo Odd News parser <br>";
+	echo "The Sun Showbizz parser <br>";
 	$html = Pharse::file_get_dom('https://www.thesun.co.uk/tvandshowbiz/');
 	// Find all the paragraph tags with a class attribute and print the
-// value of the class attribute
-var_dump($html('h2[class="teaser__headline theme__copy-color"]'));
+	// value of the class attribute
+	//var_dump($html('h2[class="teaser__headline theme__copy-color"]'));
 	foreach($html('h2[class="teaser__headline theme__copy-color"]') as $element) {
 		//var_dump($element);
 
 		$vnos = $element->getPlainText();
-	//	var_dump($vnos);
-//		echo "_____<br>";
-	//	$vnos2 = preg_replace("react-text","a",$vnos);
-	///	var_dump($vnos2);
+		//	var_dump($vnos);
+		//		echo "_____<br>";
+		//	$vnos2 = preg_replace("react-text","a",$vnos);
+		///	var_dump($vnos2);
 		/*str_ireplace(' news', '', $vnos);
 		str_ireplace(' news ', '', $vnos);
 		str_ireplace('-- react-text --', '', $vnos);
 		str_ireplace('--/react-text --', '', $vnos);*/
 		if(str_word_count($vnos)>4){
-			$first4words = implode(' ', array_slice(str_word_count($vnos,1), 0, 4));
-			$vnos = $first4words;
-			$vnos = $vnos . " ...";
+		$first4words = implode(' ', array_slice(str_word_count($vnos,1), 0, 4));
+		$vnos = $first4words;
+		$vnos = $vnos . " ...";
 		}
 		echo $vnos."<br>";
 		//echo $vnos;
-//		$returnList[] = $vnos;
-	//}
+		//		$returnList[] = $vnos;
+		//}
 
-	echo "Pharser konec";
-	//foreach($returnList as $vns){
-	//	echo $vns."<br>";
-	//}
+		echo "Pharser konec";
+		//foreach($returnList as $vns){
+		//	echo $vns."<br>";
+	}
 	//return $returnList;
 ?>
