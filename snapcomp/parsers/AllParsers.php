fda7a40192e68d665ed5d5b30cb9bc64e3bd1a24
Catacomba <kevinsuc16@gmail.com>
2017-06-12 17:07:29 +0200

    as

--- snapcomp/parsers/AllParsers.php
+++ snapcomp/parsers/AllParsers.php
@@ -5,31 +5,34 @@
   include('TheSunParser.php');
   include('TheSunShowbizParser.php');
 
-  $servername = "localhost";
-  $username = "user";
-  $password = "joomladb";
-  $dbname = "snapcomp";
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  $conn->query("TRUNCATE TABLE DAILY_SUGGESTION;");
+  //$servername = "localhost";
+  //$username = "user";
+  //$password = "joomladb";
+  //$dbname = "snapcomp";
+  //$conn = new mysqli($servername, $username, $password, $dbname);
+  //$conn->query("TRUNCATE TABLE DAILY_SUGGESTION;");
   $AllData = [];
-  if ($conn->connect_error)
-  {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  if($stmt = mysqli_prepare($conn,"INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES (?,?);"))
-	{
+  $AllData[] = TheGuardianParser();
+  $AllData[] = TheSunParser();
+  $AllData[] = TheSunShowbizzParser();
+  //if ($conn->connect_error)
+  //{
+  //  die("Connection failed: " . $conn->connect_error);
+  //}
+  //if($stmt = mysqli_prepare($conn,"INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES (?,?);"))
+	//{
       //echo "TheGuardianParserVIncludu";
-    $AllData[] = TheGuardianParser();
-    $AllData[] = TheSunParser();
-    $AllData[] = TheSunShowbizzParser();
+   // $AllData[] = TheGuardianParser();
+  //  $AllData[] = TheSunParser();
+  //  $AllData[] = TheSunShowbizzParser();
       /*
 				mysqli_stmt_bind_param($stmt,"ss",$vnos,$source);
 				mysqli_stmt_execute($stmt);
         */
-	}
-	else
-		echo "Error mysqli_prepare ni deloval!";
-	$conn->close();
+	//}
+	//else
+	//	echo "Error mysqli_prepare ni deloval!";
+//	$conn->close();
   var_dump($AllData);
 	echo "All parsers finished. The database should be updated.";
 ?>
