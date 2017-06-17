fc7ee49e65ea99b296893b435cec22c284080bc3
Catacomba <kevinsuc16@gmail.com>
2017-06-17 17:23:47 +0200

    asd

--- snapcomp/views/users/highscores.php
+++ snapcomp/views/users/highscores.php
@@ -1,9 +1,9 @@
-<div style="width: 100px">
+<div style="width: 300px">
 	<ul class="list-group">
 		<?php 
 			
 			foreach($user as $usr){
-				echo '<li class="list-group-item" style="width: 100px">'.$usr["USERNAME"].' <span class="badge">'.$usr["NUMOFWINS"].'</span></li>';
+				echo '<li class="list-group-item" style="width: 300px">'.$usr["USERNAME"].' <span class="badge">'.$usr["NUMOFWINS"].'</span></li>';
 			}
 		
 		?>
