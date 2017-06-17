1fb583b825702840237053219ac22bc2fec8912e
Catacomba <kevinsuc16@gmail.com>
2017-06-17 10:20:53 +0200

    Parserstest1

--- snapcomp/parsers/AllParsers.php
+++ snapcomp/parsers/AllParsers.php
@@ -1,55 +1,33 @@
 <?php
-  include('../../parser/pharse/pharse.php');
-  //include 'RedditRandomParser.php';
-  include 'TheGuardianParser.php';
-  include 'TheSunParser.php';
-  include 'TheSunShowbizParser.php';
+	include('../../parser/pharse/pharse.php');
+	//include 'RedditRandomParser.php';
+	include 'TheGuardianParser.php';
+	include 'TheSunParser.php';
+	include 'TheSunShowbizParser.php';
 
-  //$servername = "localhost";
-  //$username = "user";
-  //$password = "joomladb";
-  //$dbname = "snapcomp";
-  //$conn = new mysqli($servername, $username, $password, $dbname);
-  //$conn->query("TRUNCATE TABLE DAILY_SUGGESTION;");
-  $AllData = [];
-  //$TheGrd = [];
-  //$TheSn = []; 
-  //$TheSnBz = []; 
-  //$TheGrd[] = TheGuardianParser();
-  //$TheSn[] = TheSunParser();
-  //$TheSnBz[] = TheSunShowbizzParser();
-  //if ($conn->connect_error)
-  //{
-  //  die("Connection failed: " . $conn->connect_error);
-  //}
-  //if($stmt = mysqli_prepare($conn,"INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES (?,?);"))
-	//{
-      //echo "TheGuardianParserVIncludu";
-   // $AllData[] = TheGuardianParser();
-  //  $AllData[] = TheSunParser();
-  //  $AllData[] = TheSunShowbizzParser();
-      /*
-				mysqli_stmt_bind_param($stmt,"ss",$vnos,$source);
-				mysqli_stmt_execute($stmt);
-        */
-	//}
-	//else
-	//	echo "Error mysqli_prepare ni deloval!";
-//	$conn->close();
-
-	//var_dump($TheGrd);
-	//var_dump(ShowbizzFunkcija());
-
+	$servername = "localhost";
+	$username = "user";
+    $password = "joomladb";
+	$dbname = "snapcomp";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn->query("TRUNCATE TABLE DAILY_SUGGESTION;");
+	$AllData = [];
 	$result = array_merge($AllData, GuardianFunkcija());
 	$result = array_merge($result, ShowbizzFunkcija());
 	$result = array_merge($result, SunFunkcija());
-	
-	
-	echo '<pre>' . var_export($result, true) . '</pre>';
-	//echo '<pre>' . var_export(ShowbizzFunkcija(), true) . '</pre>';
-	//echo '<pre>' . var_export(SunFunkcija(), true) . '</pre>';
-	//var_dump(SunFunkcija());
-	//var_dump(GuardianFunkcija());
-    //var_dump($AllData);
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	if($stmt = mysqli_prepare($conn,"INSERT INTO DAILY_SUGGESTION (INFO, SOURCE) VALUES (?,?);"))
+	{
+		foreach($result as $entry){
+			$vnos = $entry["INFO"];
+			$source = $entry["SOURCE"];
+			mysqli_stmt_bind_param($stmt,"ss",$vnos,$source);
+			mysqli_stmt_execute($stmt);
+		}
+
+	}  
 	echo "All parsers finished. The database should be updated.";
 ?>
