e7b5489becad18e5b95541c5101d36652a6ead44
Catacomba <kevinsuc16@gmail.com>
2017-06-18 17:26:30 +0200

    asd

--- snapcomp/views/pages/home.php
+++ snapcomp/views/pages/home.php
@@ -4,11 +4,7 @@
 
 
   <hr>
-  <p style="P.blocktext {
-    margin-left: auto;
-    margin-right: auto;
-    width: 6em
-}">
+  <p style="margin-left: auto; margin-right: auto; width: 400px">
     SnapComp is a real-time photography competition. It consists of two parts, the first one being the users' profile containg basic information.
     The second part is the game itself. The user must first register, providing some necessary information such as the username, account name and alike.
     The user can also browse winning pictures, without an acccount.
