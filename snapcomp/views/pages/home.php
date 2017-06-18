08c9cc1177f53d9cf534bd0fff6cb27a65318565
ApollyonDS <miha.vres69@gmail.com>
2017-06-18 15:51:24 +0200

    fddf

--- snapcomp/views/pages/home.php
+++ snapcomp/views/pages/home.php
@@ -1,13 +1,17 @@
 <!--Enostaven pogled, ki prikazuje, kako lahko uporabimo vrednosti spremenljivk, ki smo jih določili v kontrolerju -->
-<h2>About SnapComp - <p><font face="Satisfy">"Pick a pic"</font></p></h2>
-<hr>
-<p style="margin:5px;">
-  SnapComp is a real-time photography competition. It consists of two parts, the first one being the users' profile containg basic information.
-  The second part is the game itself. The user must first register, providing some necessary information such as the username, account name and alike.
-  The user can also browse winning pictures, without an acccount.
+<h2>About SnapComp<h2>
+<p><font face="Satisfy" size="4">"Pick a pic"</font></p>
 
-  <h4>The Game</h4>
-  The game consists of room with sessions. The users can enter the room, where they can compete with other players by posting photgraphs relevant
-  to the current theme. The room has two types of players, a selector and competitors. The selectors have the ability to select the theme for the next
-  round at the start of a session and choose the best picture at the end of the session. The competitors are the ones, who take the pictures.
-</p>
+<div style="margin:5px;" class="">
+  <hr>
+  <p>
+    SnapComp is a real-time photography competition. It consists of two parts, the first one being the users' profile containg basic information.
+    The second part is the game itself. The user must first register, providing some necessary information such as the username, account name and alike.
+    The user can also browse winning pictures, without an acccount.
+    <br>
+    <h4>The Game</h4>
+    The game consists of room with sessions. The users can enter the room, where they can compete with other players by posting photgraphs relevant
+    to the current theme. The room has two types of players, a selector and competitors. The selectors have the ability to select the theme for the next
+    round at the start of a session and choose the best picture at the end of the session. The competitors are the ones, who take the pictures.
+  </p>
+</div>
