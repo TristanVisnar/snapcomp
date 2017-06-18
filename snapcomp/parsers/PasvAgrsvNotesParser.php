bcad59b454b9f8becdef01ea6aa4d7501f8723d1
ApollyonDS <miha.vres69@gmail.com>
2017-06-18 09:59:59 +0200

    tr

--- snapcomp/parsers/PasvAgrsvNotesParser.php
+++ snapcomp/parsers/PasvAgrsvNotesParser.php
@@ -5,22 +5,23 @@
 		for ($x = 2; $x <= 7; $x++)
 			{
 				$html = Pharse::file_get_dom('http://www.passiveaggressivenotes.com/page/'.$x.'/');
-				foreach($html('a[rel="bookmark"]') as $element) {
+				foreach($html('a[rel="bookmark"]') as $element)
+				{
 					$vnos = $element->getPlainText();
 					$source = "www.passiveaggressivenotes.com";
-					if(str_word_count($vnos)>4){
+
+					if(str_word_count($vnos)>4)
+					{
 						$first4words = implode(' ', array_slice(str_word_count($vnos,1), 0, 4));
 						$vnos = $first4words;
 						$vnos = $vnos . " ...";
 					}
+					
 					$list[] = array("INFO" => $vnos, "SOURCE" => $source);
 				}
-
-
 			}
-			//var_dump($list);
-			return $list;
+			$new_arr = array_unique($list, SORT_REGULAR);
+			return $new_arr;
 			//echo "passiveaggressivenotes parser finished!<br>";
 	}
-	echo '<pre>'.var_export(PSNParser()).'</pre>';
 ?>
