59466b6006c9e237645738822ee6786f62aeadc7
ApollyonDS <miha.vres69@gmail.com>
2017-06-18 10:00:34 +0200

    cv

--- snapcomp/parsers/AllParsers.php
+++ snapcomp/parsers/AllParsers.php
@@ -4,7 +4,7 @@
 	include 'TheGuardianParser.php';
 	include 'TheSunParser.php';
 	include 'TheSunShowbizParser.php';
-	//include 'PasvAgrsvNotesParser.php';
+	include 'PasvAgrsvNotesParser.php';
 
 	$servername = "localhost";
 	$username = "user";
@@ -16,7 +16,7 @@
 	$result = array_merge($AllData, GuardianFunkcija());
 	$result = array_merge($result, ShowbizzFunkcija());
 	$result = array_merge($result, SunFunkcija());
-	//$result = array_merge($result, PSNParser());
+	$result = array_merge($result, PSNParser());
 	if ($conn->connect_error)
 	{
 		die("Connection failed: " . $conn->connect_error);
